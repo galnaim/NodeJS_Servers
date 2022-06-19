ee6765b180ff0c84de0dde62354142f5035083f9
Gal Naim <dorbanp@gmail.com>
2022-06-19 15:58:07 +0300

    can read  & write NO server

--- main.js
+++ main.js
@@ -7,7 +7,6 @@
 import * as http from "http";
 import * as fs from "fs/promises";
 
-
 // http
 //   .createServer((request, response) => {
 //     response.writeHead(200, { "Content-Type": "application/json" });
@@ -22,21 +21,22 @@ function write(fileName, translation) {
     .catch((err) => console.log("Error", err));
 }
 
-function findWordInDictionary(word)
-fs.readFile("./translations.json", "utf8").then((data) => {
-  const wordsArr = JSON.parse(data);
-  {
-    if (wordsArr[0].hasOwnProperty(word))
-    {write("./he.txt", wordsArr[0][word])}
-  }
+function findWordInDictionary(word) {
+  fs.readFile("./translations.json", "utf8").then((data) => {
+      const wordsArr = JSON.parse(data);
+    {
+      wordsArr[0].hasOwnProperty(word)
+        ? write("./he.txt", wordsArr[0][word])
+        : console.log("word not found");
+    }
   });
+}
 
 function read(fileName) {
   fs.readFile(`./${fileName}`, "utf8")
     .then((wordToTranlate) => {
-        const word = JSNO.parse(wordToTranlate)
-        findWordInDictionary(word)
-        })
+      findWordInDictionary(wordToTranlate);
+    })
     .catch((err) => console.log("Erorr", err));
 }
-read("./en.txt");
+read("en.txt");
